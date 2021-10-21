ceaf62c4cc916e958185511ef5a555856d1584f4
David Peña <dpenah@intergrupo.com>
2021-10-21 11:44:03 -0500

    Feature: Update general utils

--- src/utils/messages.py
+++ src/utils/messages.py
@@ -3,5 +3,4 @@ from dataclasses import dataclass
 
 @dataclass
 class LogMessage:
-    create: str = 'New register has been created.'
-    error: str = 'Error inserting record.'
+    created: str = 'Error log will be reported'
