1b3cfcb4e13e97dc80972a2e3ddc1be0f3d90236
David Peña <dpenah@intergrupo.com>
2021-10-21 11:43:45 -0500

    Feature: Update api routes and configuration

--- src/api.py
+++ src/api.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
+from starlette.status import HTTP_200_OK
 
 from src.config import fastApiConfig
 from src.db import MongoEngine
-from src.routes import log_routes
+from src.models import NewRegister
+from src.use_cases import CreateLog
+from src.utils import LogMessage, UJSONResponse
 
 app = FastAPI(**fastApiConfig)
 db = MongoEngine().get_connection()
 
-app.include_router(log_routes)
+
+@app.post("/error")
+def create_log_error(error: NewRegister, task: BackgroundTasks):
+    task.add_task(CreateLog.handle, error)
+    return UJSONResponse(LogMessage.created, HTTP_200_OK)
